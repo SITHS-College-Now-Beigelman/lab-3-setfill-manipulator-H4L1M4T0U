72245a0aa3b4c3fde92e28200600f851d2ec28a2
H4L1M4T0U <144480724+H4L1M4T0U@users.noreply.github.com>
2024-09-30 16:28:15 -0400

    asdasd
    sadasd

---
+++ Diaby_H_Lab3.cpp
@@ -0,0 +1,35 @@
+// Halimatou Diaby
+//Homework One Part One
+//9/23/24
+
+
+
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()// main function🎀
+{
+
+
+
+
+
+cout << "#" << setfill('#') << setw(75) << "#" << endl;
+cout << "#                                                                         # "<< endl;
+cout << "# Ways to access the Task Manager on your Windows computer:               #" << endl;
+cout << "#                                                                         #" << endl;
+cout << "# Press the key combination Ctrl + Shift + Escape                         #" << endl;
+cout << "#                                                                         #"<< endl;
+cout << "#Press  the key Combination Crtl + + Alt + Delete and Select Task Manager #" << endl;
+cout << "#                                                                         #" << endl;
+cout << "#Type Task manager in the Windows Start menu search                       #" << endl;
+cout << "#                                                                         #" << endl;
+cout <<"#                                                                          #" << endl;
+cout << "###########################################################################" << endl;
+}
+
+
+
